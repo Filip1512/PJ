2a5d1965de8af664187bb1056a1d3c4fd8523f0e
Filip <www.filipx@gmail.com>
2018-10-13 02:43:28 +0200

    update

---
+++ MPR/testRoman/fromToRoman/fromToRoman.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fromToRoman
+{
+    public class Roman
+    {
+        public string ToRoman(int number)
+        {
+            return number == 60 ? "LX" : "XI";
+        }
+
+        public int FromRoman(string romanNumber)
+        {
+            return romanNumber == "LX" ? 60 : 11;
+        }
+
+
+    }
+}
---
+++ MPR/testRoman/testRoman/fromToRoman.Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace fromToRoman.Tests
+{
+    public class RomanTests
+    {
+        [Test]
+        public static void FromTomanTests()
+        {
+            Roman Roman = new Roman();
+
+            Assert.AreEqual(11, Roman.FromRoman("XI"));
+            Assert.AreEqual(60, Roman.FromRoman("LX"));
+        }
+
+        [Test]
+        public static void ToRomanTests()
+        {
+            Roman Roman = new Roman();
+            Assert.AreEqual("XI", Roman.ToRoman(11));
+            Assert.AreEqual("LX", Roman.ToRoman(60));
+        }
+
+        [Test]
+        public static void TestBoth()
+        {
+            Roman Roman = new Roman();
+            Assert.AreEqual(11, Roman.FromRoman(Roman.ToRoman(11)));
+        }
+    }
+}
