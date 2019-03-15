e4fafd52d047d9508ced53b5957befcb7f45daa3
Filip <www.filipx@gmail.com>
2019-03-15 23:55:39 +0100

    Zadania z  cwiczen, servlety, testy

--- JAZ-Java/lab01_ratyKredytu/src/main/java/servlets/HarmonogramServlet.java
+++ JAZ-Java/lab01_ratyKredytu/src/main/java/servlets/HarmonogramServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/harmonogram")
+@WebServlet("/harmonogram.jsp")
 public class HarmonogramServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String kwotaKredytuS = request.getParameter("kwotaKredytu");
---
+++ JAZ-Java/lab01_ratyKredytu/src/main/webapp/harmonogram.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Filip
+  Date: 2019-03-15
+  Time: 20:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Harmonogram spłat kredytu</title>
+</head>
+<body>
+
+</body>
+</html>
