bdc03469e11f03f98f7bb9fe4d1684c735d565e5
Filip <www.filipx@gmail.com>
2020-10-18 12:08:33 +0200

    Update repo

---
+++ TAU-Java/zad01/NWW/test/zad01/NWWTest.java
@@ -0,0 +1,22 @@
+package zad01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class NWWTest {
+	
+	@Before
+	public void setUp() {
+		nww = new NWW();
+		System.out.println("Before");
+		}
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
