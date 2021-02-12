6c4598987ee43d6c361b1f472c845b66e7edd19f
Filip <www.filipx@gmail.com>
2021-02-12 01:33:41 +0100

    Dodanie zadania z TAU - RestAPI / RIOT API

--- TAU-Java/RestAPI/main.py
+++ TAU-Java/RestAPI/main.py
@@ -1,15 +1,16 @@
 import requests
 import json
 
-country_cases = 'Poland'
-url = "https://covid-api.mmediagroup.fr/v1/cases?country={}".format(country_cases)
+locale_cases = "Europe"
+apiKey = "RGAPI-f2659fcf-4c21-4c72-880a-73a6079c0250"
+url = "https://europe.api.riotgames.com/lor/status/v1/platform-data?api_key=" + apiKey
 head = {'content-type': 'application/json'}
 response = requests.request("GET", url)
 a = json.loads(response.text)
 
 # -----------------------------------
 # sprawdzanie kluczy
-expected = ['confirmed', 'recovered', 'deaths', 'country']
+expected = ['id', 'name', 'locales', 'maintenances', 'incidents']
 current = []
 
 for key, value in a.items():
@@ -21,14 +22,12 @@ assert expected == current
 # -----------------------------------
 # sprawdzanie wartosci
 for key, value in a.items():
-    for k, v in value.items():
-        if k == 'country':
-            country = v
-        if k == 'continent':
-            continent = v
+    for k in value.items():
+        if k == 'name':
+            name = v
 
-assert country_cases == country
-assert country_cases == 'Poland'
+assert locale_cases == v
+assert locale_cases == "Europe"
 
 # -----------------------------------
 # sprawdzanie odpowiedzi
@@ -36,6 +35,6 @@ assert response.status_code == 200
 
 # -----------------------------------
 # sprawdzanie niepoprawnego zapytania
-url = "https://covid-api.mmediagroup.fr/v1/a?country={}".format(country_cases)  # zmiana endpointu z cases na a
+url = "https://europe.api.riotgames.com/lor/status/v1/platform?api_key=" + apiKey
 response = requests.request("GET", url)
 assert response.status_code == 403
