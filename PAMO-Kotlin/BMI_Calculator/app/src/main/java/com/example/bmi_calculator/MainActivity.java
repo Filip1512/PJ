3f1f154890236c5edd8b51e285e884e389fc1dae
Filip <filip1512@gmail.com>
2021-04-10 22:52:15 +0200

    Update BMI Calc app for PAMO

--- PAMO-Kotlin/BMI_Calculator/app/src/main/java/com/example/bmi_calculator/MainActivity.java
+++ PAMO-Kotlin/BMI_Calculator/app/src/main/java/com/example/bmi_calculator/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.bmi_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.bmi_calculator.MESSAGE";
 
     private EditText height;
     private EditText weight;
@@ -32,33 +34,55 @@ public class MainActivity extends AppCompatActivity {
             float heightValue = Float.parseFloat(heightStr);
             float weightValue = Float.parseFloat(weightStr);
 
-            float bmi = weightValue / (heightValue * heightValue);
+            float bmi = weightValue / (heightValue * heightValue) * (100*100);
+            // 100*100 -> to count BMI we need height in meters, so I do recalculate from centimeters
 
             displayBMI(bmi);
         }
     }
 
+    String dietToGainWeight = "\r\nYou should gain you weight! Here is example diet:\n"
+            + "Posiłek 1\n Omlet, Płatki owsiane, Rodzynki\n"
+            + "Posiłek 2\n Pierś z kurczaka, Ryż brązowy, Olej kokosowy, Świeże pomidory\n"
+            + "Posiłek 3\n Pierś z kurczaka, Kasza jaglana, Olej kokosowy, Świeży ogórek\n"
+            + "Posiłek 4\n Polędwica wołowa, Ryż biały, Ogórki kiszone\n"
+            + "Posiłek 5\n Twaróg chudy, Olej kokosowy, Świeża papryka, rzodkiewka, szczypiorek (łącznie) 250g";
+
+    String dietIsOk = "\r\nYour diet is pretty good!";
+
+    String dietToLoseWeight = "\r\nYou should lose some weight! Here is example diet:\n"
+            + "Posiłek 1\n Jogurt naturalny, 2 kanapki z przysmakiem wołowym, pomidorem i sałatą, herbata lub kawa bez cukru albo 2 kanapki ze schabem i papryką\n"
+            + "Posiłek 2\n 2 kanapki z szynką i żółtym serem, ogórek, banan albo kanapka z bułki grahamki ze schabem, sałata i kiwi"
+            + "Posiłek 3\n Plaster szynki pieczonej, ziemniaki, marchewka z groszkiem, surówka (np. z cykorii) albo zraz z kaszą jęczmienną, fasolka szparagowa, surówka (np. z kapusty pekińskiej)"
+            + "posiłek 4\n Jogurt owocowy, 2 kanapki z polędwicą, owoce albo jogurt naturalny, 2 kanapki z przysmakiem wołowym i pomidorem";
+
+
     public void displayBMI(float bmi){
         String bmiLabel = "";
 
-        if (Float.compare(bmi,15f) <= 0){ bmiLabel = getString(R.string.very_severely_underweight); }
+        if (Float.compare(bmi,15f) <= 0){ bmiLabel = getString(R.string.very_severely_underweight) + dietToGainWeight; }
         else if (Float.compare(bmi,15f) > 0 && Float.compare(bmi,16f) <= 0)
-        { bmiLabel = getString((R.string.severely_underweight)); }
+        { bmiLabel = getString((R.string.severely_underweight)) + dietToGainWeight; }
         else if (Float.compare(bmi,16f) > 0 && Float.compare(bmi,18.5f) <= 0)
-        { bmiLabel = getString((R.string.underweight)); }
+        { bmiLabel = getString((R.string.underweight)) + dietToGainWeight; }
         else if (Float.compare(bmi,18.5f) > 0 && Float.compare(bmi,25f) <= 0)
-        { bmiLabel = getString((R.string.normal)); }
+        { bmiLabel = getString((R.string.normal)) + dietIsOk; }
         else if (Float.compare(bmi,25f) > 0 && Float.compare(bmi,30f) <= 0)
-        { bmiLabel = getString((R.string.overweight)); }
+        { bmiLabel = getString((R.string.overweight)) + dietToLoseWeight; }
         else if (Float.compare(bmi,30f) > 0 && Float.compare(bmi,35f) <= 0)
-        { bmiLabel = getString((R.string.obese_class_i)); }
+        { bmiLabel = getString((R.string.obese_class_i)) + dietToLoseWeight; }
         else if (Float.compare(bmi,35f) > 0 && Float.compare(bmi,40f) <= 0)
-        { bmiLabel = getString((R.string.obese_class_ii)); }
+        { bmiLabel = getString((R.string.obese_class_ii)) + dietToLoseWeight; }
         else
         { bmiLabel = getString((R.string.obese_class_iii)); }
 
         bmiLabel = bmi + "\n\n" + bmiLabel;
         result.setText(bmiLabel);
 
+    }
+
+    public void openSummaryActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
---
+++ PAMO-Kotlin/BMI_Calculator/app/src/main/java/com/example/bmi_calculator/activity_welcome.java
@@ -0,0 +1,29 @@
+package com.example.bmi_calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class activity_welcome extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
